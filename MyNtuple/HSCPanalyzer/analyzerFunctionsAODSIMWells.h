9949e4e68b1aa039248cc3d40c82f9c48f221388
Teresa Lenz <teresa.lenz@desy.de>
2014-09-18 14:29:10 +0200

    add tau, muon electron specific funtion

--- MyNtuple/HSCPanalyzer/analyzerFunctionsAODSIMWells.h
+++ MyNtuple/HSCPanalyzer/analyzerFunctionsAODSIMWells.h
@@ -29,7 +29,69 @@ bool isTrackCaloIsolated(struct Track_s *track){
 }
 
 //--------------------------------------------------------------------------------------------------
+bool isTrackReconstructedTau(struct Track_s* track){
 
+  double dPhi = 0;
+  double dEta = 0;
+  double dR   = 0; 
+
+
+  for(unsigned int i=0; i<Tau.size(); i++){
+
+    if(Tau[i].pt<30)                                    continue;
+    if(abs(Tau[i].eta)>2.3)                             continue;
+    if(Tau[i].byLooseCombinedIsolationDeltaBetaCorr<=0) continue;
+    if(Tau[i].decayModeFinding<=0)                      continue;
+    if(Tau[i].againstElectronLoose<=0)                  continue;
+    if(Tau[i].againstMuonTight<=0)                      continue;
+
+    dPhi = std::abs(TVector2::Phi_mpi_pi(track->phi-Tau[i].phi));
+    dEta = std::abs(track->eta - Tau[i].eta);
+    dR   = std::sqrt(dPhi*dPhi + dEta*dEta); 
+
+    if(dR<0.15) return true;
+  }
+
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
+bool isTrackReconstructedElectron(struct Track_s* track){
+
+  double dPhi = 0;
+  double dEta = 0;
+  double dR   = 0; 
+
+  for(unsigned int i=0; i<Electron.size(); i++){
+
+    if(Electron[i].mvaNonTrigV0<0) continue;
+    dPhi = std::abs(TVector2::Phi_mpi_pi(track->phi-Electron[i].phi));
+    dEta = std::abs(track->eta - Electron[i].eta);
+    dR   = std::sqrt(dPhi*dPhi + dEta*dEta); 
+
+    if(dR<0.15) return true;
+  }
+
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
+bool isTrackReconstructedMuon(struct Track_s* track){
+
+  double dPhi = 0;
+  double dEta = 0;
+  double dR   = 0; 
+
+  for(unsigned int i=0; i<Muon.size(); i++){
+    
+    dPhi = std::abs(TVector2::Phi_mpi_pi(track->phi-Muon[i].phi));
+    dEta = std::abs(track->eta - Muon[i].eta);
+    dR   = std::sqrt(dPhi*dPhi + dEta*dEta); 
+
+    if(dR<0.15) return true;
+  }
+
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
 bool isTrackReconstructedLepton(struct Track_s* track){
 
   double dPhi = 0;
@@ -57,7 +119,6 @@ bool isTrackReconstructedLepton(struct Track_s* track){
 
   return false;
 }
-
 //--------------------------------------------------------------------------------------------------
 
 #endif
